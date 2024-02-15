0e8fcada22dada7d614630b393c91221a2de32b1
Nikolay <koliamba_5@mail.ru>
2024-02-15 22:27:28 +0800

    Initial

---
+++ src/main/java/pro/sky/Course2StreamApiToEmployee/Course2StreamApiToEmployeeApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.Course2StreamApiToEmployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2StreamApiToEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course2StreamApiToEmployeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/Course2StreamApiToEmployee/Course2StreamApiToEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Course2StreamApiToEmployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2StreamApiToEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
